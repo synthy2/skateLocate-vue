e0694181f5741a626e83e2a503f278403c441470
Ben S <allobon@gmail.com>
2017-03-02 14:55:35 +0000

    initial skeleton

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<h2>It works!</h2>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+	import { mapGetters }	from "vuex";
+
+	export default {
+		name: "app",
+	}
+</script>
+
+<style lang="scss">
+
+  $background: #817a7a;
+  $fontColour: #3c3838;
+
+  body {
+    background: $background;
+    color: $fontColour;
+  }
+
+  h2 {
+    letter-spacing: 12px;
+  }
+
+</style>
---
+++ src/components/Map/MapsView.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <main>
+    <section>
+      I am an amazing map!!!
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
